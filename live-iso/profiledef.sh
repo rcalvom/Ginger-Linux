303636b90b47580a7d5889fd964efc5ae029069c
Ricardo Calvo <rcalvom@unal.edu.co>
2024-01-09 08:25:46 +0000

    Added: Desktop Manager in archlive

--- live-iso/profiledef.sh
+++ live-iso/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="ginger-linux"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_publisher="Ginger Linux"
+iso_application="Ginger Linux Live/Rescue CD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
